9a56b3fe8513767ce0621f3609aad398f3d78e10
Toby Drane <tobydrane@gmail.com>
2020-08-25 17:34:54 +0100

    home screen work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image/withIEPolyfill';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 import Layout from '../components/layout';
 import Sidebar from '../components/sidebar';
@@ -12,44 +14,56 @@ const Home = ({ data }) => {
       <main className="main">
         <Sidebar />
         <div className="home-content">
-          <div className="banner">
-            <h1>Hello <img alt="waving hand" src="https://twemoji.maxcdn.com/2/svg/1f44b.svg"/> I'm Toby</h1>
-            <h2>Current MSc Data Science & Machine Learning Student at UCL</h2>
-            <p>I'm a fullstack software developer, machine learning researcher and entrepreneur from Cambridge.</p>
+          <div className="hero">
+            <div className="banner">
+              <h1>Hello <img alt="waving hand" src="https://twemoji.maxcdn.com/2/svg/1f44b.svg"/> I'm Toby</h1>
+              <h2>Current MSc Data Science & Machine Learning Student at UCL</h2>
+              <p>Software Engineer, Machine Learning researcher and Entrepreneur from Cambridge & London.</p>
+              <br />
+              <p>I've worked as fullstack developer at some of Cambridge's most exciting startups. I have specialised in frontend design and cloud architectures. Please see my blogs about software engineering, resarch and everything in-between.</p>
 
-            <div className="locations">
-              <h2>Previously at: </h2>
-              <Img
-                fluid={data.TXP.childImageSharp.fluid}
-                style={{
-                  width: "4rem",
-                  height: "4rem",
-                  marginRight: "0.5rem",
-                  zIndex: 1,
-                }}
-              />
-              <Img
-                fluid={data.CI.childImageSharp.fluid}
-                style={{
-                  width: "3.6rem",
-                  height: "3.6rem",
-                  marginRight: "0.5rem",
-                  zIndex: 1,
-                }}
-              />
-              <Img
-                fluid={data.EXESIOS.childImageSharp.fluid}
-                style={{
-                  width: "6rem",
-                  height: "4rem",
-                  zIndex: 1,
-                }}
-              />
-            </div>
+              <div className="locations">
+                <h2>Previously at: </h2>
+                <Img
+                  fluid={data.TXP.childImageSharp.fluid}
+                  style={{
+                    width: "4rem",
+                    height: "4rem",
+                    marginRight: "0.5rem",
+                    zIndex: 1,
+                  }}
+                />
+                <Img
+                  fluid={data.CI.childImageSharp.fluid}
+                  style={{
+                    width: "3.6rem",
+                    height: "3.6rem",
+                    marginRight: "0.5rem",
+                    zIndex: 1,
+                  }}
+                />
+                <Img
+                  fluid={data.EXESIOS.childImageSharp.fluid}
+                  style={{
+                    width: "6rem",
+                    height: "4rem",
+                    zIndex: 1,
+                  }}
+                />
+              </div>
 
-            <div className="hire-contact">
-              <button className="button-hire">I'm Available. Hire Me</button>
+              <div className="hire-contact">
+                <button className="button-hire">I'm Available. Hire Me</button>
+              </div>
             </div>
+            <div className="scroll-icon">
+              <a href="#features">
+                <FontAwesomeIcon className="bounce" size='2x' icon={faAngleDown} />
+              </a>
+            </div>
+          </div>
+          <div className="hero-blogs" id="features">
+            <p>Somewhere else</p>
           </div>
         </div>
       </main>
