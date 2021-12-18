51843c117b91fdc1fc9e9fbd7d30659be5eeac94
Toby Drane <tobydrane@gmail.com>
2021-12-18 13:15:52 +0000

    Revert "initial beat the bookies pipeline work"
    This reverts commit 9091d7af43f38e176a7870579eed19af0c7b1038.

--- projects/beat-the-bookies/data-process/deploy_function.sh
+++
@@ -1,5 +0,0 @@
-pipenv run pip freeze > requirements.txt
-gcloud functions deploy beat-the-bookies-data-process \
---runtime python39 \
---trigger-resource betting-algorithms \
---trigger-event google.storage.object.finalize
--- projects/beat-the-bookies/data-scrape/deploy_image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t data-scrape .
-docker tag data-scrape:latest gcr.io/tobydrane-personal-site/data-scrape
-docker push gcr.io/tobydrane-personal-site/data-scrape
