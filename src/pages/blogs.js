c1988650bb5c93cf0dd85df381f87084fdbdd2be
Toby Drane <tobydrane@gmail.com>
2022-10-14 16:36:02 +0100

    stash

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 
+import sanityClient from '../services/sanity'
+
 const Blogs = () => {
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `
+        *[_type == "post"]{
+          title,
+          slug,
+          description,
+          published,
+          mainImage{
+            asset->{
+            _id,
+            url
+          }
+        }
+      }
+    `
+      )
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
   return (
     <Layout>
       <SEO />
---
+++ src/services/sanity.js
@@ -0,0 +1,8 @@
+import sanityClient from '@sanity/client'
+
+export default sanityClient({
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
+  useCdn: true,
+  apiVersion: '2022-08-13',
+})
