d38a22066a0279bfb88206cc33a841b88a6b6bfe
Toby Drane <tdrane@no10.gov.uk>
2022-12-28 18:10:54 +0000

    fix build

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,50 +17,6 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   }
 }
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  const query = graphql(`
-    query {
-      allMdx(sort: { order: DESC, fields: [frontmatter___date] }) {
-        edges {
-          node {
-            id
-            body
-            excerpt(pruneLength: 250)
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-              date
-              type
-              private
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  return query.then((results, errors) => {
-    if (errors) {
-      return Promise.reject(errors)
-    }
-
-    // For every page
-    const blogs = results.data.allMdx.edges
-    blogs.forEach(({ node }) => {
-      createPage({
-        path: node.fields.slug,
-        component: path.resolve('./src/templates/blogs.js'),
-        context: {
-          slug: node.fields.slug,
-        },
-      })
-    })
-  })
-}
-
 // This should fix the issue with fetch and firebase throwing an error when Gatsby build
 // see - https://github.com/gatsbyjs/gatsby/issues/29012
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
